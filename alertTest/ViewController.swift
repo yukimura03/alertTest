f1dae8fc703af117bce6f0a7658d583cf2ca0223
yukimura03 <4leaf.caffe.latte@gmail.com>
2018-11-06 23:47:46 +0900

    first commit

---
+++ alertTest/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  alertTest
+//
+//  Created by minagi on 2018/11/06.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func tapButton(_ sender: Any) {
+        if textField.text == "" {
+            message = "氏名は、必須項目です"
+            myAlert()
+        } else if (textField.text?.characters.count)! >= 16 {
+            message = "氏名は、16文字以内で入力してください"
+            myAlert()
+        } else {
+            label.text = textField.text
+            textField.text = ""
+        }
+    }
+    
+    var message = ""
+    
+    func myAlert() {
+        let alert = UIAlertController(title: "警告", message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.placeholder = "氏名"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
